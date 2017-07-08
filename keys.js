f7d6af225b6ad35e5de31a4509997afacaf84fb4
Bryce Stockman <brycestockman@gmail.com>
2017-07-07 20:20:23 -0400

    progess

---
+++ keys.js
@@ -0,0 +1,8 @@
+// console.log('Keys.js is loaded.')
+
+exports.twitterKeys = {
+  consumer_key: 'qAW4QR3GYl6c4oLJ9tKJMyVZ2',
+  consumer_secret: 'mnRb832wpAPWOqbxHg2eIis4PGxL86ztuoA6yAJSCoEaqMkWZC',
+  access_token_key: '883464723282440193-2fd0baGcMcbrXdAzGolH0hqnE5fynKj',
+  access_token_secret: 'KmqRrZEc6spudrrDngTdct4AIIYY3lQQx5jZSrJyUyiLi'
+}
---
+++ liri.js
@@ -0,0 +1,36 @@
+var keys = require('./keys.js');
+
+var Twitter = require('twitter');
+
+var getMyTweets = function() {
+
+  var client = new Twitter(keys.twitterKeys);
+
+  var params = {screen_name: 'pearlstockman'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(' ');
+        console.log("\"" + tweets[i].text + "\"");
+        console.log(tweets[i].created_at);
+        console.log(' ');
+      }
+    }
+  });
+}
+
+var pick = function(caseData, functionData) {
+  switch(caseData) {
+    case 'my-tweets':
+      getMyTweets();
+      break;
+    default:
+    console.log('LIRI does not know that');
+  }
+}
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
